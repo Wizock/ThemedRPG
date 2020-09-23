fedc741c9810a633d2a111ac0679c57b2a405352
Wizock <Rohaanerodasahmed@gmail.com>
2020-09-23 14:21:35 +1000

    changed sprite size need to work on the transforming feature

--- level1.py
+++ level1.py
@@ -4,12 +4,14 @@ from mainWindow import *
 class lvlOne:
     mainWindow()
     def __init__(self):
-
-        self.hero = makeSprite("sprites/hero/straight.png")
+        self.hero = makeSprite("H:\python\Themed RPG\ThemedRPG\sprites\hero\straght.png")
     def DisplayLvl(self):
         hideAll()
         hideLabel(pre.msg)
         hideLabel(pre.contLabel)
+        self.a = list((1920,1080))
+        self.b = list(pre.resolution)
+        transformSprite(self.hero,(self.b[0]/self.a[0])+(self.b[1]/self.a[1]),0)
         showSprite(self.hero)
 
     
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from preWindow import *
 from level1 import *
 from mainWindow import *
 
-levelone = lvlOne()
-levelone.DisplayLvl()
+level = lvlOne()
+level.DisplayLvl()
 
 while True:
     if keyPressed("space"):
