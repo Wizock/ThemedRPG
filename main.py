ade43a82dd39149098f195f606f0ee51644f0900
unknown <tristan.green.300604@gmail.com>
2020-09-23 18:25:48 +1000

    b bbbbb

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from initalise import *
 
 while True:
     if keyPressed("w"):
-        moveSprite(level.hero,)
+        moveSprite(level.hero,1, 1)
     if keyPressed("space"):
         end()
 
+"""fuck this shit bron janjf"""
